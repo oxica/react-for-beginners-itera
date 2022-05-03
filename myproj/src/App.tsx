385665a7614036126cde492d96d63ead05fad6a2
Oksana <kskaplun@gmail.com>
2022-05-03 17:26:56 +0300

    user-info

--- myproj/src/App.tsx
+++ myproj/src/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
 import { Footer } from "./Footer";
+import info from "./info.json";
+import { Profile } from "./Profile";
 
-const user = { userName: "Vitalii" };
+const user = { userName: "Oksana" };
 
 const App = () => (
   <div className="App">
-    <h1>Hello world {user.userName}</h1>
+    <h1>Hello! I'm {user.userName}</h1>
+    <Profile
+      username={info.username}
+      hobby={info.hobby}
+      location={info.location}
+      github={info.github}
+      linkedin={info.linkedin}
+    />
     <Footer copyright="C" />
   </div>
 );
